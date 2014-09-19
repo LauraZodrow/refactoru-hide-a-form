09dd5d42552df4e88848602d0ed6194e39135c4f
Laura <laura.zodrow@gmail.com>
2014-09-19 16:52:16 -0600

    another commit

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 $(document).on('ready', function() {
- 	$('form').on('submit', function() {
- 		$('input').atr('data-target').text(input.val());
- 		$(this).val();
+ 	$('form').css('display', 'none');
+ 	$('form').on('submit', function(e) {
+ 		var original = $($('input').attr('data-target')).text($('input').val());
+		var newText = $('.profile-page'). find('p').val(original);
+ 		// $(this).text(newText);
+ 		e.preventDefault();
  	});
 
- 	$('form').on('submit', function(e) {
-  		e.preventDefault();
-	});
+ 	$('.show-form').on('click', function() {
+ 		$('form').toggle();
+ 		$('.show-form').text("Hide Form");
+ 	});
 
 });
 
 
-// ($('input').atr("data-target")).text(input.val())
+// var original = $($('input').attr('data-target')).text($('input').val());
+// var newText = $('.profile-page'). find('p').val(original);
+
+
 
   // $('form').on('submit', function() {
   // 	$('textarea').text(update);
+
+  // var newText = $(this).val();
+ 	// 	$('p').text(newText);
  
 
 
