7ac75f34f386491896ceb4f8070d73c7ec48c46d
Laura <laura.zodrow@gmail.com>
2014-09-19 12:36:07 -0600

    second commit

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 $(document).on('ready', function() {
  	$('form').on('submit', function() {
- 		var original = $('input').val();
- 		var update = $('p').val(orginal);
+ 		$('input').atr('data-target').text(input.val());
+ 		$(this).val();
  	});
 
  	$('form').on('submit', function(e) {
-  	e.preventDefault();
+  		e.preventDefault();
 	});
 
 });
 
 
+// ($('input').atr("data-target")).text(input.val())
 
   // $('form').on('submit', function() {
   // 	$('textarea').text(update);
